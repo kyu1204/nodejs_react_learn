aca0c8abb32bc3574fd41f41c28887f3fb2dab1b
김민규 <minkyu@gimmingyuui-MacBookPro.local>
2021-08-13 10:05:21 +0900

    login 기능 추가

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const config = require('./config/key');
-
+const cookieParser = require('cookie-parser')
 const { User } = require('./modles/User');
 
 
@@ -20,15 +20,39 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err})
+    if (err) return res.json({ success: false, err })
     return res.status(200).json({ success: true })
   })
 })
 
+app.post('/api/users/login', (req, res) => {
+  // 요청된 email 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({ loginSuccess: false, message: 'auth error' })
+    }
+
+    // 요청된 email의 password 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "auth error" })
+
+      // 토큰 생성하기
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err)
+
+        // 토큰을 저장한다 (쿠키, 로컬스토리지, 세션스토리지)
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- boiler-plate/modles/User.js
+++ boiler-plate/modles/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +34,46 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', function (next) {
+    var user = this
 
-module.exports = {User}
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+
+                user.password = hash
+                next()
+            })
+        })
+    }
+    else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this
+
+    // jwt token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = { User }
