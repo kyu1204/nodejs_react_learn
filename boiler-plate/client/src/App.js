9740ee5de7739e4cb3ccd16dd5592f80c3e42b64
김민규 <minkyu@gimmingyuui-MacBookPro.local>
2021-08-17 17:35:38 +0900

    React boiler form 정리, react router dom 셋팅

--- boiler-plate/client/src/App.js
+++ boiler-plate/client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/register' component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div>
+            LadingPage
+        </div>
+    )
+}
+
+export default LandingPage
