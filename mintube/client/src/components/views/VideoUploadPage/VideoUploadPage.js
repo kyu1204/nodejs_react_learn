656819500ae8f6f9d056571592e53d46c9f831fe
김민규 <minkyu@gimmingyuui-MacBookPro.local>
2021-08-19 15:21:57 +0900

    viedeo upload onchage 셋팅 및 dropzone 셋팅

--- mintube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ mintube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import DropZone from 'react-dropzone'
 
 const { TextArea } = Input
 const { Title } = Typography
 
+const PrivateOptions = [
+    { value: 0, label: 'Private' },
+    { value: 1, label: 'Public' }
+]
+
+const CategoryOptions = [
+    { value: 0, label: 'Film & Animation' },
+    { value: 1, label: 'Autos & Vehicles' },
+    { value: 2, label: 'Music' },
+    { value: 3, label: 'Pets & Animals' },
+    { value: 4, label: 'Movies' },
+    { value: 5, label: 'Games' }
+]
+
 function VideoUploadPage() {
+    const [VideoTitle, setVideoTitle] = useState('')
+    const [Description, setDescription] = useState('')
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState('Film & Animation')
+
+    const onTitleChange = (event) => {
+        setVideoTitle(event.currentTarget.value)
+    }
+
+    const onDescriptionChange = (event) => {
+        setDescription(event.currentTarget.value)
+    }
+
+    const onPrivateChange = (event) => {
+        setPrivate(event.currentTarget.value)
+    }
+
+    const onCategoryChange = (event) => {
+        setCategory(event.currentTarget.value)
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -39,29 +74,42 @@ function VideoUploadPage() {
 
                 <br />
                 <br />
+
                 <label>Title</label>
                 <Input
-                    onChange
-                    value />
+                    onChange={onTitleChange}
+                    value={VideoTitle} />
+
                 <br />
                 <br />
+
                 <label>Description</label>
                 <TextArea
-                    onChange
-                    value
+                    onChange={onDescriptionChange}
+                    value={Description}
                 />
+
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
+
+                <select onChange={onPrivateChange}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
+
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
+
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
+
                 <br />
                 <br />
+
                 <Button type='primary' size='large' onClick>
                     Submit
                 </Button>
